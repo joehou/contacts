12c1092a4a1de706f92f2b97708e90de938c90dc
alsjoe <tyranthou@gmail.com>
2017-06-28 17:57:42 -0700

    check in 3-7

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends Component {
-    render(){
-        const people = this.props.contacts;
-        return (
-            <ol>
-                {people.map( person =>(
-                    <li key={person.name}>{person.name}</li>
-                ))}
-            </ol>
-        )
-    }
-}
+// import './App.css';
+import ListContacts from './ListContacts';
 
 
 class App extends Component {
-  render() {
+  state = {
+      contacts: [
+          {
+              "id": "ryan",
+              "name": "Ryan Florence",
+              "email": "ryan@reacttraining.com",
+              "avatarURL": "http://localhost:5001/ryan.jpg"
+          },
+          {
+              "id": "michael",
+              "name": "Michael Jackson",
+              "email": "michael@reacttraining.com",
+              "avatarURL": "http://localhost:5001/michael.jpg"
+          },
+          {
+              "id": "tyler",
+              "name": "Tyler McGinnis",
+              "email": "tyler@reacttraining.com",
+              "avatarURL": "http://localhost:5001/tyler.jpg"
+          }
+      ]
+  }
+
+  removeContact = contact =>{
+      console.log('removing contact');
+      this.setState( (state)=>({
+            contacts: state.contacts.filter( c => c.id !== contact.id)
+          })
+      )
+  }
+
+    render() {
     return (
       <div className="App">
-        <ContactList contacts = {[{name:'joe'},{name:'frank'} ]} />
-        <ContactList contacts = {[{name:'donkeyboi'},{name:'hashpiper'} ]} />
+         <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact}/>
       </div>
     );
   }
